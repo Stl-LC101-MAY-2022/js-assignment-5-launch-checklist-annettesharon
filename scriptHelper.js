1701cc5f3adab0c148290e3c0d8456d5463aaf0f
annettesharon <annettesharon@ymail.com>
2022-07-31 23:28:37 -0500

    Changes in myFetch functon

--- scriptHelper.js
+++ scriptHelper.js
@@ -87,10 +87,10 @@ function updateShuttle(e, pilotName, copilotName, fuelLevel, cargoMass) {
 async function myFetch() {
     let planetsReturned;
 
-    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
+    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
         response.json().then(function (json) {
             pickPlanet(planetsReturned);
-});
+}));
 
     return pickPlanet(planetsReturned);;
 }
