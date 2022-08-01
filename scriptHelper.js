b143922c5f210fc8b61727fd26a0292e02ccb976
annettesharon <annettesharon@ymail.com>
2022-08-01 01:26:45 -0500

    edit myFetch

--- scriptHelper.js
+++ scriptHelper.js
@@ -88,7 +88,7 @@ async function myFetch() {
     let planetsReturned;
 
     planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
-         return planetsReturned.json();
+         return response.planetsReturned();
            
 });
 }
